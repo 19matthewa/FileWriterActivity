93660f1253d0658004fc6f4075a3c3833b42d921
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2024-09-06 14:44:37 -0700

    fixed silliy
    changed the string

--- sillyClass.java
+++ sillyClass.java
@@ -1,6 +1,6 @@
 public class sillyClass {
     public static void main(String[] args) {
-        System.out.println("This is a silly class");
+        System.out.println("This is Not a silly class");
         //fun
     }
 }
